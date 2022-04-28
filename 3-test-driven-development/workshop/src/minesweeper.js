86703f8dc8c0f3b6c05905cc64e31b0ce9520102
marceaudavid <marceau.david@ynov.com>
2022-04-28 14:15:13 +0200

    functionnal minesweeper

--- 3-test-driven-development/workshop/src/minesweeper.js
+++ 3-test-driven-development/workshop/src/minesweeper.js
@@ -1,14 +1,53 @@
-function minesweeper(input) {
-  let splitted = input.split(/[0-9 ]+/gm);
-  let count = 0;
-  for (let step = 0; step < splitted.length; step++) {
-    if (splitted[step] !== "") {
-      count++;
-      splitted.splice(step - 1, 0, `Field #${count}:`);
+function minesweeper(board) {
+  let result = [];
+  for (let i = 0; i < board.length; i++) {
+    let row = [];
+    for (let j = 0; j < board[i].length; j++) {
+      let count = 0;
+      for (let x = -1; x <= 1; x++) {
+        for (let y = -1; y <= 1; y++) {
+          if (
+            i + x >= 0 &&
+            i + x < board.length &&
+            j + y >= 0 &&
+            j + y < board[i].length
+          ) {
+            if (board[i + x][j + y] === "*") {
+              count++;
+            }
+          }
+        }
+      }
+      if (board[i][j] === "*") {
+        row.push("*");
+      } else {
+        row.push(count);
+      }
     }
+    result.push(row);
   }
-  return splitted.join("");
+  return result.map((row) => row.join("")).join("\n");
 }
+
+function parseString(input) {
+  let fields = input.split(/[0-9 ]+/g).filter((field) => field);
+  let sizes = input.match(/[0-9 ]+/g);
+  sizes.pop();
+  const hints = fields.map((field, i) => {
+    let array = field.split("");
+    let board = [];
+    let size = parseInt(sizes[i][2]);
+    for (let x = 0; x < array.length; x += size) {
+      let line = array.slice(x, x + size);
+      board.push(line);
+    }
+    let result = minesweeper(board);
+    return `Field #${i + 1}:\n${result}\n`;
+  });
+  return hints.join("");
+}
+
+console.log(parseString("4 4*........*......3 5**.........*...0 0"));
 
 module.exports = {
   minesweeper,
